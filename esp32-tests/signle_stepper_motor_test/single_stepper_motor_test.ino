1d7339466990fc8a1ecd9c2e3fd17d107bf9d0f6
Sanjit Raman <51057192+SanjitRaman@users.noreply.github.com>
2024-05-21 23:34:15 +0100

    Add header and expected test results.

--- esp32-tests/signle_stepper_motor_test/single_stepper_motor_test.ino
+++ esp32-tests/signle_stepper_motor_test/single_stepper_motor_test.ino
@@ -1,3 +1,16 @@
+// Single Stepper Motor Test
+// Author: Sanjit Raman
+// Date: 21 May 2024
+// Version: 1.0.0
+// Description:
+//    This code is meant to work with an A4988 motor driver connected to the
+//    microcontroller with the pinouts as described above. The expected result:
+//        - the stepper motor spins FORWARDS for 1 second
+//        - waits for 1 second
+//        - the stepper motor spins BACKWARDS for 1 second
+//        - at the end the motor should return to the same position it started
+//        at.
+
 // Define pin connections & motor's steps per revolution
 const int dirPin = 2;
 const int stepPin = 3;
