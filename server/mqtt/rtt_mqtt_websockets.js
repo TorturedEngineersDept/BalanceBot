a0537efb4ce9c0151c07158af198c78242e2dfdf
Sara Chehab <s.edu@chehab.co>
2024-05-23 21:15:56 +0100

    Measuring RTT for MQTT

---
+++ server/mqtt/rtt_mqtt_websockets.js
@@ -0,0 +1,88 @@
+const mqtt = require('mqtt');
+
+let INTERVAL_STEERING_UPDATE = 200;
+let currentSpeed = 0;
+let currentAngle = 0;
+
+const MQTT_BROKER = '35.177.90.106';
+const MQTT_PORT = 8000;
+
+// Connect to the MQTT broker via WebSockets
+const client = mqtt.connect(`ws://${MQTT_BROKER}:${MQTT_PORT}`, {
+    reconnectPeriod: 1000, // Reconnect after 1 second
+    connectTimeout: 30 * 1000, // 30 seconds timeout for connecting
+    keepalive: 60 // 60 seconds keep-alive ping
+});
+
+const topic = 'test/topic';
+const message = 'test message';
+const iterations = 1000;
+
+let sendTimes = [];
+let receiveTimes = [];
+let completed = 0;
+
+client.on('connect', () => {
+    console.log('Connected to MQTT broker');
+
+    client.subscribe(topic, (err) => {
+        if (err) {
+            console.error('Subscription error:', err);
+            process.exit(1);
+        } else {
+            console.log('Subscribed to topic');
+            measureTime();
+        }
+    });
+});
+
+client.on('message', (topic, message) => {
+    receiveTimes.push(Date.now());
+
+    completed++;
+    if (completed < iterations) {
+        measureTime();
+    } else {
+        calculateAverageTime();
+        client.end();
+    }
+});
+
+
+// ----------------- CALCULATING RTT -----------------
+
+function measureTime() {
+    const sendTime = Date.now();
+    sendTimes.push(sendTime);
+    client.publish(topic, message);
+}
+
+function calculateAverageTime() {
+    let totalLatency = 0;
+
+    for (let i = 0; i < iterations; i++) {
+        const latency = receiveTimes[i] - sendTimes[i];
+        totalLatency += latency;
+    }
+
+    const averageLatency = totalLatency / iterations;
+    console.log(`Average latency over ${iterations} iterations: ${averageLatency} ms`);
+}
+
+// ----------------- MAINTAINING CONNECTION -----------------
+
+client.on('error', function (error) {
+    console.error('Connection error:', error);
+});
+
+client.on('close', () => {
+    console.log('MQTT client disconnected');
+});
+
+client.on('reconnect', () => {
+    console.log('MQTT client reconnecting...');
+});
+
+client.on('offline', () => {
+    console.log('MQTT client offline');
+});
