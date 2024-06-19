f3d61df34ce85fce419a7dcc3347c560fd6e5a44
Sanjit Raman <51057192+SanjitRaman@users.noreply.github.com>
2024-06-19 17:11:59 +0100

    exported layers, created css `@keyframes` animation.

--- server/website/src/pages/Home.js
+++ server/website/src/pages/Home.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import './Home.css';
+import img1 from '../images/1.png';
+import img2 from '../images/2.png';
+import img3 from '../images/3.png';
+import img4 from '../images/4.png';
+import img5 from '../images/5.png';
 
 const Home = () => {
     const navigate = useNavigate();
